7ac76e5df21f2d86aec897503f5b45671c82b3d2
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-11 16:57:53 +0600

    Task 4 in HW3 partmly completed

--- HW3-4/Program.cs
+++ HW3-4/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Задача 4 - Подсчитать сумму цифр в числе
 
+// Определить количество цифр в числе
 
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine());
+
+int i = 0;
+
+while (N>0)
+{
+    i++;
+    N/=10;
+}
+Console.WriteLine("Количество чисел: "+ i);
