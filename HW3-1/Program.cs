257c225098f48c34d8c1dc9d7afd3d1d5152834f
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-11 16:08:53 +0600

    Task 1 in HW 3 completed

---
+++ HW3-1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 1 - Найти кубы чисел от 1 до N
+
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i} x {i} x {i} = {i*i*i}");
+}
+Console.WriteLine();
