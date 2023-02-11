c02ce964683f9fe0b621c464bda9c96ef088d02b
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-11 16:20:59 +0600

    Task 3 in HW3 partly completed

---
+++ HW3-3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 3 - Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine("Введите A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите B: ");
+int B = int.Parse(Console.ReadLine());
+
+double pow = Math.Pow(A, B);
+
+Console.WriteLine(pow);
