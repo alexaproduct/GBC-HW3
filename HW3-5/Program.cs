e4ba7fec13431a3bba4c3fb33c735645f9c117c4
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-12 14:24:41 +0600

    Task 5 in HW3 completed

--- HW3-5/Program.cs
+++ HW3-5/Program.cs
@@ -1,1 +1,15 @@
 ﻿// Задача 5 - Написать программу вычисления произведения чисел от 1 до N
+
+
+// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите A: ");
+int A = int.Parse(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 1; i <= A; i++)
+{
+    result = result*i;
+}
+Console.WriteLine(result);
