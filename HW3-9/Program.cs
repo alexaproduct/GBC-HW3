0c4d199fa20b1839ceef97bca1e935ebc2954301
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-12 23:19:28 +0600

    Task 9 in HW3 added

---
+++ HW3-9/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 9 - Задать массив, заполнить случайными 
+// положительными трёхзначными числами. 
+
+int[] arr = new int[16];
+
+for (int i = 0; i <= 15; i++)
+{
+    arr[i] = new Random().Next(100, 1000);
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine();
