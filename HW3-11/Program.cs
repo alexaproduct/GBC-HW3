80464dffdc3357c52d77ef859944246023882f4e
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-12 23:35:04 +0600

    Task 11 in HW3 added

---
+++ HW3-11/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 11 - В одномерном массиве из 123 
+// чисел найти количество элементов из 
+// отрезка [10,99]
+
+int[] arr = new int[123];
+
+for (int i = 0; i <= 122; i++)
+{
+    arr[i] = new Random().Next(0, 200);
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine();
+
+int count = 0;
+
+for (int i = 0; i <= 122; i++)
+{
+    if (arr[i] >= 10 && arr[i] <= 99)
+    {
+        count = count + 1;
+    }
+}
+Console.WriteLine("Кол-во количество элементов из отрезка [10,99]: " + count);
