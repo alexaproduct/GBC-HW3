35297f164793d84a05f12986633521ec9e305409
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-12 23:42:34 +0600

    Task 12 in HW3 added

---
+++ HW3-12/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 12 - Найти сумму чисел 
+// одномерного массива
+// стоящих на нечетной позиции
+
+int[] arr = new int[7];
+
+for (int i = 0; i <= 6; i++)
+{
+    arr[i] = new Random().Next(0, 10);
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine();
+
+int sumoddi = 0;
+
+for (int i = 0; i <= 6; i++)
+{
+    if (i%2 != 0)
+    {
+        sumoddi = sumoddi + arr[i];
+    }
+}
+Console.WriteLine("Сумма на нечетных позициях: " + sumoddi);
