c7ec3fd07063371952b8f29416f5d462a70c0da9
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-13 00:07:51 +0600

    Task 13 in HW3 added

---
+++ HW3-13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13 - Найти произведение пар чисел 
+// в одномерном массиве. 
+// Парой считаем первый и последний элемент, 
+// второй и предпоследний и т.д.
+
+
+int[] arr = new int[7];
+
+for (int i = 0; i <= 6; i++)
+{
+    arr[i] = new Random().Next(0, 10);
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine();
+
+int product = 1;
+int j = 7;
+
+for (int i = 0; i <= 6; i++)
+{
+    j = j-1;
+    product = arr[i] * arr [j];
+    Console.WriteLine(product);
+    if (i>=j) break;
+}
+Console.WriteLine();
