1a4b832f3f32265aac55f7cccb2e34faa39a883c
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-12 23:15:44 +0600

    Task 8 in HW3 added

---
+++ HW3-8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8 - Определить, присутствует ли 
+// в заданном массиве, некоторое число
+
+int[] arr = new int[10];
+
+for (int i = 0; i <= 9; i++)
+{
+    arr[i] = new Random().Next(0, 20);
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine();
+
+for (int i = 0; i <= 9; i++)
+{
+    if (arr[i] == 19) Console.WriteLine("Да, присутствует");
+}
